364b64963786895a268d92415d2aeb6521e953a1
bowen-zhu <bowen.zhu@binance.com>
2022-09-06 11:59:05 +0400

    feat: 🎸 init vite & project

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import * as path from 'path'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+  ],
+  define: {
+    __DEV__: isDev,
+    __TEST__: false,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: isDev ? 3001 : 3000,
+  },
+})
