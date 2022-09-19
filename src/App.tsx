326e1f4349ffc5325c9f2d07e484a31def1fb9b6
stannis-x <stannis.x@binance.com>
2022-09-19 16:50:45 +0800

    feat: change js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
-import Header from '@/components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Header /> */}
       <button
         className={'m-2 p-2 border rounded text-red-500 font-bold'}
         onClick={() => setCount((v) => v + 1)}
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { Web3Provider } from '@ethersproject/providers'
-import { Web3ReactProvider } from '@web3-react/core'
 import App from '@/App'
 import '@/style/index.scss'
-
-const getLibrary = (provider: any) => {
-  return new Web3Provider(provider)
-}
-
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
